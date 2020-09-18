870329bc0db11f8919e8663869ebdce711ac8c3e
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-09-18 16:49:06 -0700

    chinese

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let debug = false;
+let debug = true;
 let script = [];
 let personalities = [];
 let say;
@@ -10,14 +10,14 @@ $.get(path, (data) => {
   say = data.say;
   script = script.concat(data.directions);
   script = script.concat($.map(data.questions, function(i) {
-    return say + ' ' + i;
+    return say + '<span class="phrase">' + i + '</span>';
   }));
   script = script.concat($.map(data.script, function(i) {
-    return say + ' ' + i;
+    return say + '<span class="phrase">' + i + '</span>';
   }));
-  update();
-  setInterval(update, debug ? 500 : 5000);
   $('#text').text(data.init.toUpperCase()).fitText();
+  setInterval(update, debug ? 500 : 5000);
+  update();
 });
 
 const update = () => {
@@ -25,7 +25,7 @@ const update = () => {
   if (Math.random() < rand) {
     let item = randomItem(script);
     item = item.replace('___', randomItem(personalities));
-    $('#text').text(item.toUpperCase()).fitText();
+    $('#text').html(item.toUpperCase()).fitText();
   }
 }
 
