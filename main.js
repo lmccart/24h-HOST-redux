c4376c1b0f1926518503ce5af23c3594d14f3767
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-09-17 23:14:38 -0700

    ok

--- main.js
+++ main.js
@@ -12,14 +12,16 @@ $.get(path, (data) => {
   script = script.concat($.map(data.questions, function(i) {
     return say + ' ' + i;
   }));
-  script = script.concat(data.script);
+  script = script.concat($.map(data.script, function(i) {
+    return say + ' ' + i;
+  }));
   update();
   setInterval(update, debug ? 500 : 5000);
   $('#text').text(data.init.toUpperCase()).fitText();
 });
 
 const update = () => {
-  let rand = debug ? 1 : 0.5;
+  let rand = debug ? 1 : 0.75;
   if (Math.random() < rand) {
     let item = randomItem(script);
     item = item.replace('___', randomItem(personalities));
