b90ea465d9701201188340fa6b48115f23c4ba8b
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-09-17 17:07:46 -0700

    ok

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
+let debug = true;
 let script = [];
 let personalities = [];
+let say;
 
 let path = document.documentElement.lang === 'en' ? 'script-en.json' : 'script-zh.json';
 $.get(path, (data) => {
   console.log(data);
   personalities = data.personalities;
+  say = data.say;
   script = script.concat(data.directions);
-  script = script.concat(data.questions);
+  script = script.concat($.map(data.questions, function(i) {
+    return say + ' ' + i;
+  }));
   script = script.concat(data.script);
   update();
-  setInterval(update, 5000);
+  setInterval(update, debug ? 500 : 5000);
   $('#text').text(data.init.toUpperCase()).fitText();
 });
 
 const update = () => {
   let item = '';
-  if (Math.random() < 0.5) {
+  let rand = debug ? 1 : 0.5;
+  if (Math.random() < rand) {
     item = randomItem(script);
     item = item.replace('___', randomItem(personalities));
   }
